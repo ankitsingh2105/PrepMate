9329290cb7f3a2bca5d0ba9f0770f7f64d10cfd2
Ankit <ankitchauhan21500@gmail.com>
2025-06-09 11:47:36 +0530

    link updated

--- BackEnd/server.js
+++ BackEnd/server.js
@@ -14,14 +14,14 @@ const connect = require("./connect");
 const server = http.createServer(app);
 const io = new Server(server, {
   cors: {
-    origin: ["http://localhost:5173", "https://prep-mate-one.vercel.app"],
+    origin: ["http://localhost:5173", "https://prep-mate-one.vercel.app", "https://prepmatee.vercel.app"],
     credentials: true
   }
 });
 
 // Middlewares
 app.use(cors({
-  origin: ["http://localhost:5173", "https://prep-mate-one.vercel.app"],
+  origin: ["http://localhost:5173", "https://prep-mate-one.vercel.app", "https://prepmatee.vercel.app"],
   credentials: true
 }));
 app.use(express.json());
