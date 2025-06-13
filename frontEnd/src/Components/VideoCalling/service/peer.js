b497ded563ad92acb44663ee4d26a5bbe9ac6762
Ankit <ankitchauhan21500@gmail.com>
2025-06-14 01:35:22 +0530

    added comments

--- frontEnd/src/Components/VideoCalling/service/peer.js
+++ frontEnd/src/Components/VideoCalling/service/peer.js
@@ -1,5 +1,8 @@
 class PeerService {
     constructor() {
+        // Creates a WebRTC peer connection.
+        // Google and Twilio’s STUN servers to get public IPs for NAT traversal.
+        // Listens for .onicecandidate, and when a new candidate is found, it calls a callback you set  via setOnIceCandidate().
         this.webRTCPeer = new RTCPeerConnection({
             iceServers: [
                 {
@@ -12,7 +15,6 @@ class PeerService {
         });
         this.webRTCPeer.onicecandidate = (event) => {
             if (event.candidate) {
-                console.log("New ICE candidate:", event.candidate);
                 this.onIceCandidate?.(event.candidate);
             }
         };
@@ -22,19 +24,25 @@ class PeerService {
         this.onIceCandidate = callback;
     }
 
-    async getOffer() {
+    async makeOffer() {
         const offer = await this.webRTCPeer.createOffer();
+        // this localDescription of the one calling
         await this.webRTCPeer.setLocalDescription(new RTCSessionDescription(offer));
+        // i will send this using web-sockets
         return offer;
     }
 
     async getAnswer(offer) {
+        // offer i made above 
         await this.webRTCPeer.setRemoteDescription(new RTCSessionDescription(offer));
+        // making an SDP
         const ans = await this.webRTCPeer.createAnswer();
+        // this localDescription of the the one picking up the call
         await this.webRTCPeer.setLocalDescription(new RTCSessionDescription(ans));
         return ans;
     }
 
+    // used for negotiation : when we switch camera, on/off microphonee
     async setRemoteDescription(ans) {
         await this.webRTCPeer.setRemoteDescription(new RTCSessionDescription(ans));
     }
@@ -42,7 +50,6 @@ class PeerService {
     async addIceCandidate(candidate) {
         try {
             await this.webRTCPeer.addIceCandidate(new RTCIceCandidate(candidate));
-            console.log("Added ICE candidate:", candidate);
         } catch (error) {
             console.error("Error adding ICE candidate:", error);
         }
@@ -71,3 +78,4 @@ class PeerService {
 }
 
 export default new PeerService();
+
