f97721f7cd217d4f86462fd0490d2ccf14d675ac
Ankit Singh Chauhan <ankitchauhan21500@gmail.com>
2025-08-19 20:49:28 +0530

    ended the process

--- BackEnd/cleanUp.js
+++ BackEnd/cleanUp.js
@@ -3,13 +3,14 @@ const mongoose = require("mongoose");
 const mockModel = require("./Model/mockModel");
 
 
-async function cleanUp(){
+async function cleanUp() {
     await mongoose.connect("mongodb+srv://WHQMCNBYGhTTwIHN:ankitchauhan21500@cluster0.2ipp9om.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0");
-    
+
     const today = new Date();
     console.log(today);
-    const result = await mockModel.deleteMany({schedule : {$lt : today}});
+    const result = await mockModel.deleteMany({ schedule: { $lt: today } });
     console.log(result);
-    // await find
+    await mongoose.disconnect();
+    process.exit(0);
 }
 cleanUp(); 
