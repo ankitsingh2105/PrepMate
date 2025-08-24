eb6c4a94dcd524980cd8bc83b4d00f4018e569a8
Ankit Singh Chauhan <ankitchauhan21500@gmail.com>
2025-08-24 12:43:34 +0530

    added compound unique contraint

--- BackEnd/Model/mockModel.js
+++ BackEnd/Model/mockModel.js
@@ -1,23 +1,24 @@
 const mongoose = require("mongoose");
 
 const mockSchema = new mongoose.Schema({
-    mockType : {
-        type : String,
+    mockType: {
+        type: String,
     },
-    schedule : {
-        type : Date
+    schedule: {
+        type: Date
     },
-    tempLock : {
-        type : Boolean,
-        default : false,
+    tempLock: {
+        type: Boolean,
+        default: true,
     },
-    ifAddedToList : {
-        type : Boolean,
+    ifAddedToList: {
+        type: Boolean,
     },
-    user : {
-        type : mongoose.Schema.Types.ObjectId,
-        ref : "User", 
+    user: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "User",
     },
-}) 
- 
-module.exports =  mongoose.model("Mock" , mockSchema);
+})
+
+mockSchema.index({ user: 1, schedule:1, mockType: 1 }, { unique: true });
+module.exports = mongoose.model("Mock", mockSchema);
